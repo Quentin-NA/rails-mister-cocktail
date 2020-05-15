d3a558942ff9d9d380c2e368b0100bb3494363a8
quentin natoly <quentin.natoly@gmail.com>
2020-05-15 16:14:39 +0200

    Homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :cocktails do
+  get "/", to: "cocktails#index"
+  resources :cocktails, only: [:new, :show, :create, :edit, :update, :destroy] do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
