f924964d65c5b74b86fa197de7e5d6ecb6ebe6e0
quentin natoly <quentin.natoly@gmail.com>
2020-05-15 16:27:08 +0200

    root correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get "/", to: "cocktails#index"
-  resources :cocktails, only: [:new, :show, :create, :edit, :update, :destroy] do
+  root to: "cocktails#index"
+  resources :cocktails, only: [:new, :show, :create] do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
